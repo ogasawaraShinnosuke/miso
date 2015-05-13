3dd8ff44e8f3ba077170b0ea336ceba97f648721
ogasawaraShinnosuke <must.sow.before.you.can.reap+0001@gmail.com>
2015-05-13 19:21:06 +0900

    Update MailTest.java

--- test/MailTest.java
+++ test/MailTest.java
@@ -1,8 +1,10 @@
 package test.main.homepage.common;
 
+import junit.framework.Assert;
 import junit.framework.TestCase;
 import main.homepage.main.di.AdminMail;
 import main.homepage.main.di.AdvertiseMail;
+import main.homepage.main.di.CommonMail;
 import main.homepage.main.di.UserMail;
 import org.junit.Test;
 
@@ -10,16 +12,16 @@ public class MailTest extends TestCase {
 
     @Test
     public void testAdminMail() throws Exception {
-        new AdminMail();
+        Assert.assertEquals(new AdminMail().execute(), CommonMail.SUCCESS_VALUE);
     }
 
     @Test
     public void testAdvertiseMail() throws Exception {
-        new AdvertiseMail();
+        Assert.assertEquals(new AdvertiseMail().execute(), CommonMail.SUCCESS_VALUE);
     }
 
     @Test
     public void testUserMail() throws Exception {
-        new UserMail();
+        Assert.assertEquals(new UserMail().execute(), CommonMail.SUCCESS_VALUE);
     }
 }
