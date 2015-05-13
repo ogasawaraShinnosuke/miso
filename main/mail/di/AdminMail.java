ef579f624fb734cb88c42fb251a267516caacd4a
ogasawaraShinnosuke <must.sow.before.you.can.reap+0001@gmail.com>
2015-05-13 10:24:18 +0900

    Create AdminMail.java

---
+++ main/mail/di/AdminMail.java
@@ -0,0 +1,10 @@
+package main.homepage.main.di;
+
+import main.homepage.common.Mail;
+import main.homepage.mail.Standard;
+
+public class AdminMail extends CommonMail {
+    public AdminMail() {
+        super(new Standard());
+    }
+}
