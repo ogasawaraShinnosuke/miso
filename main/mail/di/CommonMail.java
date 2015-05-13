271042f4a7fc9a2fca0d2f2f2fe6005ff42c4dc0
ogasawaraShinnosuke <must.sow.before.you.can.reap+0001@gmail.com>
2015-05-13 19:18:46 +0900

    Update CommonMail.java

--- main/mail/di/CommonMail.java
+++ main/mail/di/CommonMail.java
@@ -3,10 +3,28 @@ package main.homepage.main.di;
 import main.homepage.common.Mail;
 
 public class CommonMail {
-    private void execute(Mail mail) {
+    protected void execute(Mail mail) {
         new MailDI(mail).send();
     }
     protected CommonMail(Mail mail) {
         execute(mail);
+    }
+
+    // test code.
+    public static final int SUCCESS_VALUE = 1;
+    public static final int FAIL_VALUE = 0;
+    public int execute_check(Mail mail) {
+        try {
+            execute(mail);
+        } catch (Exception e) {
+            return fail();
+        }
+        return success();
+    }
+    protected int success() {
+        return SUCCESS_VALUE;
+    }
+    protected int fail() {
+        return FAIL_VALUE;
     }
 }
