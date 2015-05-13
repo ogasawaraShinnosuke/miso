d6994aa1a1b862ce0e067b33119e2d241a34caab
ogasawaraShinnosuke <must.sow.before.you.can.reap+0001@gmail.com>
2015-05-13 10:22:54 +0900

    Create OISAsp.java

---
+++ main/mail/OISAsp.java
@@ -0,0 +1,11 @@
+package main.homepage.mail;
+
+import main.homepage.common.Asp;
+import main.homepage.common.Mail;
+
+public class OISAsp extends Asp implements Mail {
+    @Override
+    public void send() {
+        // OIS会社用のASPメール送信の実装
+    }
+}
